8f87d0a675b41642c899da50b6a13d267db3231e
werdnakof <andrewfok007@gmail.com>
2018-02-05 15:05:17 +0000

    - Updated TicTacMove with positional index and toString

--- src/TicTacToe/TicTacMove.java
+++ src/TicTacToe/TicTacMove.java
@@ -3,12 +3,24 @@ package TicTacToe;
 import AlphaBeta.Move;
 
 public class TicTacMove implements Move {
+    private Integer index;
     private TicTacPlayer playedBy;
-    public TicTacMove(TicTacPlayer playedBy) {
+
+    public TicTacMove(Integer index, TicTacPlayer playedBy) {
+        this.index = index;
         this.playedBy = playedBy;
     }
 
     public TicTacPlayer playedBy() {
         return playedBy;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return this.playedBy + " play " + this.index;
     }
 }
